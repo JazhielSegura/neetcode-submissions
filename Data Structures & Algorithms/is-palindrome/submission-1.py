cd0119a5c021160979d3cce77cb5c18f0200fd19
Jazhiel Segura <100491705+JazhielSegura@users.noreply.github.com>
2026-05-13 21:57:28 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # we want to read fowards and backwards, so we should have two pointers
+        # char.isalnum() returns true if a character is alphanumeric
+        new_str = ''.join(char for char in s if char.isalnum())
+        new_str = new_str.upper()
+        left = 0
+        right = len(new_str) - 1
+        while left < right:
+            if new_str[left] != new_str[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+        # # we also could do the cheeky python solution
+        # # first we make the input string into a single string with only alpha numeric
+        # # also need to make case insensitive
+        # new_str = ''.join(char for char in s if char.isalnum())
+        # new_str = new_str.upper()
+        # print(new_str)
+        # if new_str == new_str[::-1]:
+        #     return True
+        # return False
